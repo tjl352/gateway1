65a96e6fd74eba590d7ce5f610d1bb7d99a52e4f
Thomas Lee <tjl352@gmail.com>
2019-10-06 18:56:53 -0400

    Initial version of gateway1 generated by JHipster-6.3.1

---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Gateway1SharedLibsModule } from './shared-libs.module';
+import { JhiAlertComponent } from './alert/alert.component';
+import { JhiAlertErrorComponent } from './alert/alert-error.component';
+import { JhiLoginModalComponent } from './login/login.component';
+import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
+
+@NgModule({
+  imports: [Gateway1SharedLibsModule],
+  declarations: [JhiAlertComponent, JhiAlertErrorComponent, JhiLoginModalComponent, HasAnyAuthorityDirective],
+  entryComponents: [JhiLoginModalComponent],
+  exports: [Gateway1SharedLibsModule, JhiAlertComponent, JhiAlertErrorComponent, JhiLoginModalComponent, HasAnyAuthorityDirective]
+})
+export class Gateway1SharedModule {}
